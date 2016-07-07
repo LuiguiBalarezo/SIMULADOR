5b280cc5b6e4ae1d7ed1cae61fd49017c92372af
Sheylavc <sheylavc@hotmail.com>
2016-07-07 16:59:44 -0500

    updates svc

--- application/views/inicio/v_register.php
+++ application/views/inicio/v_register.php
@@ -91,10 +91,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								<span class="glyphicon glyphicon-lock form-control-feedback"></span>
 							</div>
 							<div class="form-group has-feedback">
-								<label style="margin-top: 20px">FECHA DE REGISTRO 07/07/2016</label>
+								<label style="margin-top: 8px">FECHA DE REGISTRO 07/07/2016</label>
 							</div>
 							<div class="checkbox icheck">
-								<label style="margin-top: 5px">
+								<label style="margin-top: 6px">
 									<input type="checkbox"> Estoy de acuerdo con los <a>Términos y condiciones</a>
 								</label>
 							</div>
