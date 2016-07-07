f441ed18e16837342141332b00260f10c7d62a58
uc-web <ucwebperu@gmail.com>
2016-07-07 18:20:58 -0500

    admin vista

--- application/controllers/panel/C_panel.php
+++ application/controllers/panel/C_panel.php
@@ -13,7 +13,38 @@ class C_Panel extends CI_Controller {
 	public function index() {
 		$this->load->view('panel');
 	}
+	public function perfil(){
 
+
+	}
+	public function estudio(){
+
+
+	}
+	public function cienpreguntas(){
+
+
+	}
+	public function completo(){
+
+
+	}
+	public function bibliografia(){
+
+
+	}
+	public function manual(){
+
+
+	}
+	public function soporte(){
+
+
+	}
+	public function licencia(){
+
+
+	}
 
 
 }
