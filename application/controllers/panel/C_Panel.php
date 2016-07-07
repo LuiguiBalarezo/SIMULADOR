aaa0e090b947c54132248ebeb3da4d79454d00c9
Sheylavc <sheylavc@hotmail.com>
2016-07-07 15:15:08 -0500

    updates svc

--- application/controllers/panel/C_Panel.php
+++ application/controllers/panel/C_Panel.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_Home extends CI_Controller {
+class C_Panel extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
