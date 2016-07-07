b0424e59bc08d4c41d6e3951bbb5f803310a82c4
uc-web <ucwebperu@gmail.com>
2016-07-07 13:29:47 -0500

    admin vista

--- application/views/admin/v-panel-menu.php
+++ application/views/admin/v-panel-menu.php
@@ -9,61 +9,48 @@
 	          <a href="panel/perfil">
 	            <i class="fa fa-user"></i> <span>EDITAR MI PERFIL</span> <i class="fa fa-angle-left pull-right"></i>
 	          </a>
-	          <ul class="treeview-menu">
-	            <li <?php echo ($submenu == 1) ? 'class="active"' : ''; ?>><a href="panel/perfil"><i class="fa fa-circle-o"></i>  </a></li>
-	            <li <?php echo ($submenu == 2) ? 'class="active"' : ''; ?>><a href="admin/tipousuario"><i class="fa fa-circle-o"></i> </a></li>
-				<li <?php echo ($submenu == 9) ? 'class="active"' : ''; ?>><a ><i class="fa fa-circle-o"></i> </a></li>
-	          </ul>
+
 	        </li>
 	        <li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-	          <a href="admin/operacion">
+	          <a href="panel/estudio">
 	            <i class="fa fa-dashboard"></i> <span>MODULO DE ESTUDIO</span> <i class="fa fa-angle-left pull-right"></i>
 	          </a>
-	          <ul class="treeview-menu">
-	            <li <?php echo ($submenu == 3) ? 'class="active"' : ''; ?>><a href="admin/operacion"><i class="fa fa-circle-o"></i> SUBIR EXCEL</a></li>
-	            <li <?php echo ($submenu == 4) ? 'class="active"' : ''; ?>><a ><i class="fa fa-circle-o"></i> VER </a></li>
-			  </ul>
+
 	        </li>
 			<li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-				<a href="admin/operacion">
+				<a href="panel/cienpreguntas">
 					<i class="fa fa-dashboard"></i> <span>SIMULADOR DE 100 PREGUNTAS</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
-				<ul class="treeview-menu">
-					<li <?php echo ($submenu == 3) ? 'class="active"' : ''; ?>><a href="admin/operacion"><i class="fa fa-circle-o"></i> SUBIR EXCEL</a></li>
-					<li <?php echo ($submenu == 4) ? 'class="active"' : ''; ?>><a ><i class="fa fa-circle-o"></i> VER </a></li>
-				</ul>
+
 			</li>
 
 			<li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-				<a href="admin/operacion">
+				<a href="panel/completo">
 					<i class="fa fa-dashboard"></i> <span>SIMULADOR DE EXAMEN (COMPLETO)</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
-				<ul class="treeview-menu">
-					<li <?php echo ($submenu == 3) ? 'class="active"' : ''; ?>><a href="admin/operacion"><i class="fa fa-circle-o"></i> USUARIO DEMO</a></li>
-					<li <?php echo ($submenu == 4) ? 'class="active"' : ''; ?>><a ><i class="fa fa-circle-o"></i> USUARIO PREMIUM </a></li>
-				</ul>
+
 			</li>
 			<li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-				<a href="admin/operacion">
+				<a href="panel/bibliografia">
 					<i class="fa fa-dashboard"></i> <span>BIBLIOGRAFIA</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
 
 			</li>
 			<li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-				<a href="admin/operacion">
+				<a href="panel/manual">
 					<i class="fa fa-dashboard"></i> <span>MANUAL DE USO</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
 
 			</li>
 			<li <?php echo ($menu == 2) ? 'class="active"' : ''; ?>>
-				<a href="admin/operacion">
+				<a href="panel/soporte">
 					<i class="fa fa-dashboard"></i> <span>SOPORTE TECNICO (AYUDA)</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
 
 			</li>
 
 			<li <?php echo ($menu == 7) ? 'class="active"' : ''; ?>>
-				<a href="admin/backup">
+				<a href="panel/licencia">
 					<i class="fa fa-floppy-o"></i> <span>COMPRAR O RENOVAR LICENCIA</span> <i class="fa fa-angle-left pull-right"></i>
 				</a>
 
