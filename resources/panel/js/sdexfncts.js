dba5b7830a8c81416c6c0fd7da56eee9f7857662
Sheylavc <sheylavc@hotmail.com>
2016-07-08 14:53:33 -0500

    updates svc

--- resources/panel/js/sdexfncts.js
+++ resources/panel/js/sdexfncts.js
@@ -212,9 +212,7 @@ $(document).ready(function(){
             var bv = $form.data('bootstrapValidator');
 
             // Use Ajax to submit form data
-            $.post($form.attr('action'), $form.serialize(), function(result) {
-                console.log(result);
-            }, 'json');
+
 
             // Use Ajax to submit form data
             //$.post(base_url+"registerIn", $('#registerForm').serialize(), function(result) {
