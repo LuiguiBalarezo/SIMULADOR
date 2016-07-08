22767a9ae397c7ef9de1fc7639d198247a9a8f09
uc-web <ucwebperu@gmail.com>
2016-07-08 12:05:06 -0500

    admin vista

--- application/controllers/panel/C_panel.php
+++ application/controllers/panel/C_panel.php
@@ -11,38 +11,38 @@ class C_Panel extends CI_Controller {
 	}
 
 	public function index() {
-		$this->load->view('panel');
+		$this->load->view('v-admin-panel');
 	}
 	public function perfil(){
-
+		$this->load->view('v-cliente-perfil');
 
 	}
 	public function estudio(){
-
+		$this->load->view('v-cliente-estudio');
 
 	}
 	public function cienpreguntas(){
-
+		$this->load->view('v-cliente-cienpreguntas');
 
 	}
 	public function completo(){
-
+		$this->load->view('v-cliente-completo');
 
 	}
 	public function bibliografia(){
-
+		$this->load->view('v-cliente-bibliografia');
 
 	}
 	public function manual(){
-
+		$this->load->view('v-cliente-manual');
 
 	}
 	public function soporte(){
-
+		$this->load->view('v-cliente-soporte');
 
 	}
 	public function licencia(){
-
+		$this->load->view('v-cliente-licencia');
 
 	}
 
