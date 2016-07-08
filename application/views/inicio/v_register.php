420b95365a5f34b693e408abfad8901a59c1e5f8
Sheylavc <sheylavc@hotmail.com>
2016-07-08 10:36:32 -0500

    updates svc

--- application/views/inicio/v_register.php
+++ application/views/inicio/v_register.php
@@ -54,8 +54,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<!-- Text input-->
 						<div class="col-md-6 col-xs-12">
 							<div class="form-group">
-								<label class="col-md-4 control-label">First Name</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class="">First Name</label>
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 										<input  name="first_name" placeholder="First Name" class="form-control"  type="text">
@@ -67,7 +67,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 							<div class="form-group">
 								<label class="col-md-4 control-label" >Last Name</label>
-								<div class="col-md-4 inputGroupContainer">
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 										<input name="last_name" placeholder="Last Name" class="form-control"  type="text">
@@ -77,8 +77,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 							<!-- Text input-->
 							<div class="form-group">
-								<label class="col-md-4 control-label">E-Mail</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class=" control-label">E-Mail</label>
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
 										<input name="email" placeholder="E-Mail Address" class="form-control"  type="text">
@@ -91,8 +91,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						<!-- Text input-->
 						<div class="col-md-6 col-xs-12">
 							<div class="form-group">
-								<label class="col-md-4 control-label">Phone #</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class=" control-label">Phone #</label>
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
 										<input name="phone" placeholder="(845)555-1212" class="form-control" type="text">
@@ -103,8 +103,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- Text input-->
 
 							<div class="form-group">
-								<label class="col-md-4 control-label">Address</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class=" control-label">Address</label>
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
 										<input name="address" placeholder="Address" class="form-control" type="text">
@@ -115,8 +115,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- Text input-->
 
 							<div class="form-group">
-								<label class="col-md-4 control-label">City</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class="control-label">City</label>
+								<div class=" inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
 										<input name="city" placeholder="city" class="form-control"  type="text">
@@ -127,8 +127,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- Select Basic -->
 
 							<div class="form-group">
-								<label class="col-md-4 control-label">State</label>
-								<div class="col-md-4 selectContainer">
+								<label class="control-label">State</label>
+								<div class="selectContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-list"></i></span>
 										<select name="state" class="form-control selectpicker" >
@@ -163,8 +163,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- Text input-->
 
 							<div class="form-group">
-								<label class="col-md-4 control-label">Zip Code</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class=" control-label">Zip Code</label>
+								<div class="inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
 										<input name="zip" placeholder="Zip Code" class="form-control"  type="text">
@@ -174,8 +174,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 							<!-- Text input-->
 							<div class="form-group">
-								<label class="col-md-4 control-label">Website or domain name</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class="control-label">Website or domain name</label>
+								<div class="inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-globe"></i></span>
 										<input name="website" placeholder="Website or domain name" class="form-control" type="text">
@@ -185,8 +185,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 							<!-- radio checks -->
 							<div class="form-group">
-								<label class="col-md-4 control-label">Do you have hosting?</label>
-								<div class="col-md-4">
+								<label class="control-label">Do you have hosting?</label>
+								<div class="">
 									<div class="radio">
 										<label>
 											<input type="radio" name="hosting" value="yes" /> Yes
@@ -203,8 +203,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 							<!-- Text area -->
 
 							<div class="form-group">
-								<label class="col-md-4 control-label">Project Description</label>
-								<div class="col-md-4 inputGroupContainer">
+								<label class="control-label">Project Description</label>
+								<div class="inputGroupContainer">
 									<div class="input-group">
 										<span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
 										<textarea class="form-control" name="comment" placeholder="Project Description"></textarea>
@@ -217,8 +217,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 						<!-- Button -->
 						<div class="form-group">
-							<label class="col-md-4 control-label"></label>
-							<div class="col-md-4">
+							<label class="control-label"></label>
+							<div class="">
 								<button type="submit" class="btn btn-warning" >Send <span class="glyphicon glyphicon-send"></span></button>
 							</div>
 						</div>
