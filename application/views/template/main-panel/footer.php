133493001339fe1140146f0567a1b0a09e533783
uc-web <ucwebperu@gmail.com>
2016-07-08 12:59:50 -0500

    admin vista

--- application/views/template/main-panel/footer.php
+++ application/views/template/main-panel/footer.php
@@ -2,8 +2,8 @@
 <footer class="main-footer">
 	<!-- To the right -->
 	<div class="pull-right hidden-xs">
-		<strong>Version</strong> 0.0 | <strong>Desarrollador</strong> <a href="http://www.servosa.pe/" target="_blank">SERVOSA</a>
+		<strong>Version</strong> 0.0 | <strong>Desarrollador</strong> <a href="http://www.simuladordeexamenes.com/" target="_blank">Simulador de Examenes</a>
 	</div>
 	<!-- Default to the left -->
-	<strong>Copyright &copy; 2015 <a href="http://www.servosa.pe/" target="_blank">SERVOSA</a>.</strong> Todos los derechos reservados.
+	<strong>Copyright &copy; 2016 <a href="http://www.simuladordeexamenes.com/" target="_blank">Simulador de Examenes</a>.</strong> Todos los derechos reservados.
 </footer>
