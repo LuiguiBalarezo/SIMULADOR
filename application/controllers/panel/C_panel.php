2d9e8e376b8597c8715e107458529af3a5088a7b
uc-web <ucwebperu@gmail.com>
2016-07-08 13:02:52 -0500

    admin vista

--- application/controllers/panel/C_panel.php
+++ application/controllers/panel/C_panel.php
@@ -11,38 +11,39 @@ class C_Panel extends CI_Controller {
 	}
 
 	public function index() {
-		$this->load->view('v-admin-panel');
+		$this->load->view('admin/cliente/v-admin-panel');
 	}
 	public function perfil(){
-		$this->load->view('v-cliente-perfil');
+		$this->load->view('admin/cliente/v-cliente-perfil');
 
 	}
 	public function estudio(){
-		$this->load->view('v-cliente-estudio');
+		$this->load->view('admin/cliente/v-cliente-estudio');
 
 	}
 	public function cienpreguntas(){
-		$this->load->view('v-cliente-cienpreguntas');
+		$this->load->view('admin/cliente/v-cliente-cienpreguntas');
 
 	}
 	public function completo(){
-		$this->load->view('v-cliente-completo');
+
+		$this->load->view('admin/cliente/v-cliente-completo');
 
 	}
 	public function bibliografia(){
-		$this->load->view('v-cliente-bibliografia');
+		$this->load->view('admin/cliente/v-cliente-bibliografia');
 
 	}
 	public function manual(){
-		$this->load->view('v-cliente-manual');
+		$this->load->view('admin/cliente/v-cliente-manual');
 
 	}
 	public function soporte(){
-		$this->load->view('v-cliente-soporte');
+		$this->load->view('admin/cliente/v-cliente-soporte');
 
 	}
 	public function licencia(){
-		$this->load->view('v-cliente-licencia');
+		$this->load->view('admin/cliente/v-cliente-licencia');
 
 	}
 
