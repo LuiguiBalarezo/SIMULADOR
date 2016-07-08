512b21d75b7f8a1a6d67d6a2b0d5b97dd404082c
uc-web <ucwebperu@gmail.com>
2016-07-08 12:02:59 -0500

    admin vista

--- application/controllers/admin/C_Admin.php
+++ application/controllers/admin/C_Admin.php
@@ -7,7 +7,7 @@ class C_Admin extends CI_Controller {
 		parent::__construct();
 		$this->load->helper('url');
 		$this->load->library('session');
-        
+
         $this->load->library('utils/UserSession');
         $this->usersession->validateSession();
 	}
@@ -19,11 +19,9 @@ class C_Admin extends CI_Controller {
 
         $usuario                    = $this->M_Admin_Usuario->getByID($this->session->id_usuario);
         $modulo->datos_usuario      = $usuario[0];
-        
+
         /* Datos de la cabecera del panel de administrador*/
         $modulo->nombres_usuario    = $usuario[0]->nombre." ".$usuario[0]->apellidos;
-
-
         
         $data["modulo"] = $modulo;
         $this->load->view('admin/v-admin-panel', $data);
