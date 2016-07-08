0dfa91375b38ed9023aa08b870269bd7d4b7782a
Sheylavc <sheylavc@hotmail.com>
2016-07-08 15:52:05 -0500

    updates svc

--- application/controllers/C_Geo.php
+++ application/controllers/C_Geo.php
@@ -7,8 +7,6 @@ class C_Geo extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
         $this->load->library('session');
-
-
     }
 
     public function getProvincias()	{
